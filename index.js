a79e89c36fceab7ff4b127fb480d400304f17db7
Shiyu Wang <shiyu3169@gmail.com>
2019-07-18 14:20:59 -0400

    team battle

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const brain = require('brain.js');
 const network = new brain.NeuralNetwork();
 
 network.train([
-  { input: [0, 0, 0], output: [0] },
-  { input: [0, 0, 1], output: [0] },
-  { input: [0, 1, 1], output: [0] },
-  { input: [1, 0, 1], output: [1] },
-  { input: [1, 1, 1], output: [1] }
+  { input: [1, 2], output: [1] },
+  { input: [1, 3], output: [1] },
+  { input: [2, 3], output: [0] },
+  { input: [2, 4], output: [1] }
 ]);
 
-const output = network.run([1, 0, 0]);
+const output = network.run([1, 4]);
 
 console.log(`Prob: ${output}`);
